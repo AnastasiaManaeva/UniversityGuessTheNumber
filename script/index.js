3f26b7cdb7463105eaa600e0812500a67483aa73
AnastasiaManaeva <tvoyasovushka@yandex.ru>
2024-11-30 23:22:34 +0500

    console.log() delete

--- script/index.js
+++ script/index.js
@@ -12,7 +12,6 @@ function handleStartGame() {
         document.getElementById('warningStart').textContent = 'Пожалуйста, введите корректные мин. и макс. значения.';
         document.getElementById('rangeA').value = '';
         document.getElementById('rangeB').value = '';
-        console.log(document.getElementById('rangeA').value);
         return;
     }
     
